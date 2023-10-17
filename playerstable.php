a750a5816eb0676f3f5d60a5dd1b18e0ad5f0326
krix-jpg <rizkytristan97@gmail.com>
2023-10-17 11:42:23 +0700

    new file:   README.md 	new file:   ajax.php 	new file:   css/style.css 	new file:   form.php 	new file:   includes/Database.php 	new file:   includes/Player.php 	new file:   index.php 	new file:   js/script.js 	new file:   logo.png 	new file:   playerstable.php 	new file:   profile.php 	new file:   style.css 	new file:   uploads/024b94fb7c158c98d2e7d86d5b7f4ea1.jpg 	new file:   uploads/0e3c41fabfbca4d81daba5a058aed5fe.jpg 	new file:   uploads/114f6affe008e1c0e0dd4977d97d815e.jpg 	new file:   uploads/1288b5636f8f092160fbdd87b921993f.jpg 	new file:   uploads/206d980fda4b562eb8eeac2832bd8581.jpg 	new file:   uploads/33b4ea918c9a487f5c50c163dcc86856.jpg 	new file:   uploads/36af33c19f843d741bf40ffa4659ec40.jpg 	new file:   uploads/384317012f4b73d88244bb04ed908107.jpg 	new file:   uploads/3c76db85104dd6e7d85b866e4087ff14.jpg 	new file:   uploads/3df523758df083b39a63398e773bda1a.png 	new file:   uploads/4f55cb62a9d6d89384ff092ecb371e8e.jpg 	new file:   uploads/5369a92797b6ceee724d1a890937e9d7.jpg 	new file:   uploads/5464ccdff544f09478c3931132e1a21c.jpg 	new file:   uploads/5910d692d0eaf712adeb8eadc1629e84.jpg 	new file:   uploads/5d526de0e84456e4fa05d4bc20332d05.jpg 	new file:   uploads/629b49c31d374ecb55a330982852fcca.jpg 	new file:   uploads/6a44fc7d915f9ae8ceb382e4973f74c2.jpg 	new file:   uploads/73043749e5aa4cfa2b42fb7ef5566fe6.jpg 	new file:   uploads/76ac4f16827d97740325790735eee696.jpg 	new file:   uploads/9253b8e93cfd60fa46b49dbb6511811e.png 	new file:   uploads/9a85d07c22aa99e14cedff0da177a78a.jpg 	new file:   uploads/a51dc17899ed0b4e550472ee92223c51.jpg 	new file:   uploads/a8016c9000e7493a1615c5fbb17b6c18.jpg 	new file:   uploads/aad504b6e576a13ecdb2d2ee3845a7b9.png 	new file:   uploads/ab172686df819afa9a2130e9906b5aa8.jpg 	new file:   uploads/ac79abf88d1ec080b0c1c8d3833e628f.png 	new file:   uploads/b0fc887ccf7eecf1bef60683dbd7cc36.png 	new file:   uploads/c39ae202434a7bf1d87c6c1fce3ab583.jpg 	new file:   uploads/c7f1cc0510b2b92801e6ef3d8142f6b3.jpg 	new file:   uploads/d5255788a21add5d151609479717c65f.jpg 	new file:   uploads/dad8de5cc4f279cf203b29fe7614b6fe.jpg 	new file:   uploads/default.png 	new file:   uploads/e021ab3bc032a09497fb70e16b22e0a1.jpg 	new file:   uploads/e60db18f9cfd872d1b3bb43e881e0b2b.jpg 	new file:   uploads/eacd545d39c601d966eb83dc700703d1.jpg 	new file:   uploads/ef3c21a3031d7416c03cbf9184cce665.png 	new file:   uploads/fa5176ed2e300e611c6c2d69c4a166e8.jpg

---
+++ playerstable.php
@@ -0,0 +1,16 @@
+<!-- table -->
+<table class="table table-hover table table-bordered text-dark" id="userstable">
+  <thead>
+    <tr>
+      <th scope="col ">Picture</th>
+      <th scope="col">ISBN</th>
+      <th scope="col">Nama Buku</th>
+      <th scope="col">Pengarang</th>
+      <th scope="col">Opsi</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+<!-- table -->
